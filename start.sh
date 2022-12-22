cb7800a06f9c8552d3fa9af75211e1dbeb487a6f
wtrknght <116600901+wtrknght@users.noreply.github.com>
2022-12-22 07:03:59 +0000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+
+sudo pacman -S firefox
+sudo pacman -S git
+sudo pacman -S python
+sudo pacman -S python-pip
+git clone https://github.com/wtrknght/arcadesetup
+cd arcadesetup
+bash arcadesetup(arch).sh
+cd ~/start
+git clone romsforretro
+python googlesign.py
+exit 0
